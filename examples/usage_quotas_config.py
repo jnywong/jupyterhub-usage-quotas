724befccd8170248006f5f7e425ced11ef767d9f
jnywong <jnywong.pro@gmail.com>
2026-02-11 12:46:46 +0000

    refactor: convert to library

--- examples/usage_quotas_config.py
+++ examples/usage_quotas_config.py
@@ -2,15 +2,6 @@
 
 c = get_config()  # noqa
 
-# Application config
-
-c.QuotasApp.server_ip = "127.0.0.1"
-c.QuotasApp.server_port = 8000
-
-c.QuotasApp.log_datefmt = "%Y-%m-%d %H:%M:%S"
-c.QuotasApp.log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-c.QuotasApp.log_level = "INFO"
-
 # Quota system config
 
 c.Quotas.prometheus_usage_metrics = [
--- src/jupyterhub_usage_quotas/app.py
+++ src/jupyterhub_usage_quotas/app.py
@@ -1,18 +1,12 @@
-import logging.config
-import os
-import signal
-
-import uvicorn
-from fastapi import FastAPI
-from traitlets import Bool, Integer, Unicode
-from traitlets.config import Application, Instance
+from traitlets import Unicode
+from traitlets.config import Application, Bool
 
 from jupyterhub_usage_quotas.config import Quotas
 
 
 class QuotasApp(Application):
     name = "jupyterhub_usage_quotas"
-    description = "Start the JupyterHub usage quotas application."
+    description = "JupyterHub usage quotas enforcement library."
     examples = """
     Generate default config file:
 
@@ -21,10 +15,6 @@ class QuotasApp(Application):
 
     # Application traits
 
-    server_ip = Unicode("0.0.0.0", help="IP address to bind API server.").tag(
-        config=True
-    )
-    server_port = Integer(8000, help="Port to bind API server.").tag(config=True)
     config_file = Unicode(
         "jupyterhub_usage_quotas_config.py", help="The config file to load"
     ).tag(config=True)
@@ -33,8 +23,6 @@ class QuotasApp(Application):
     # Configurable traits
 
     classes = [Quotas]
-
-    quotas_config = Instance(Quotas)
 
     # Aliases
 
@@ -50,70 +38,22 @@ class QuotasApp(Application):
         )
     }
 
-    def initialize(self, *args, **kwargs):
-        super().initialize(*args, **kwargs)
-        self.load_config_file(self.config_file)
-        self._format_logs()
+    def initialize(self, argv=None):
+        super().initialize(argv)
         if self.generate_config:
             config_text = self.generate_config_file()
             if isinstance(config_text, bytes):
                 config_text = config_text.decode("utf8")
-            self.log.info(f"Writing default config to: {self.config_file}")
+            print(f"Writing default config to: {self.config_file}")
             with open(self.config_file, mode="w") as f:
                 f.write(config_text)
-            self.stop()
-
-        return
-
-    def _build_app(self) -> FastAPI:
-        app = FastAPI()
-
-        @app.get("/")
-        def root():
-            return {"message": "Welcome to the JupyterHub Usage Quotas API"}
-
-        @app.get("/health/ready")
-        def ready():
-            """
-            Readiness probe endpoint.
-            """
-            return ("200: OK", 200)
-
-        return app
-
-    def _format_logs(self):
-        self.log.propagate = False
-        # Propagate uvicorn logger into central traitlets.Application logger
-        uvicorn_loggers = (
-            "uvicorn",
-            "uvicorn.error",
-            "uvicorn.access",
-        )
-        for name in uvicorn_loggers:
-            logger = logging.getLogger(name)
-            logger.propagate = True
-            logger.parent = self.log
-            logger.setLevel(self.log_level)
-        # Apply config to application logger
-        self.log.setLevel(self.log_level)
-        _formatter = logging.Formatter(fmt=self.log_format, datefmt=self.log_datefmt)
-        for handler in self.log.handlers:
-            if handler.formatter is None:
-                handler.setFormatter(_formatter)
+            return
+        if self.config_file:
+            print(f"Loading config file: {self.config_file}")
+            self.load_config_file(self.config_file)
 
     def start(self):
-        self.app = self._build_app()
-        self.initialize()
-
-        uvicorn.run(
-            self.app,
-            host=self.server_ip,
-            port=self.server_port,
-        )
-
-    def stop(self):
-        os.kill(os.getpid(), signal.SIGTERM)
-        return
+        print(f"{self.config}")
 
 
 def main():
